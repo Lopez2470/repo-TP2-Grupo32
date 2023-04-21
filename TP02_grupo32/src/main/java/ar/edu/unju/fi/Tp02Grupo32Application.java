05a4f712dc7dac2dfe70aa0b71ce883cb9f2820e
Carlos <Carlos@DESKTOP-LONCR8V>
2023-04-21 15:27:42 -0300

    Primer Commit: proyecto tp02 inicial

---
+++ TP02_grupo32/src/main/java/ar/edu/unju/fi/Tp02Grupo32Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp02Grupo32Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp02Grupo32Application.class, args);
+	}
+
+}
