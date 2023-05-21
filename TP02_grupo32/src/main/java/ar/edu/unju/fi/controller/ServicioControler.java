3f774e3929b3a63a2eda74f101de10b932501029
David Paoloni <Rx@ASUSRX>
2023-05-21 18:17:31 -0300

    Crear servicios-nuevo.html y modificación de su controller: Se crea el archivo servicios-nuevo.html para agregar nuevos objetos, en el mismo se pueden realizar modificaciones de objetos. Modificación de Serviciocontroller para realizar el alta y modificación de objetos.

--- TP02_grupo32/src/main/java/ar/edu/unju/fi/controller/ServicioControler.java
+++ TP02_grupo32/src/main/java/ar/edu/unju/fi/controller/ServicioControler.java
@@ -3,7 +3,11 @@ package ar.edu.unju.fi.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import ar.edu.unju.fi.listas.ListaServicio;
 import ar.edu.unju.fi.model.Servicio;
@@ -23,4 +27,52 @@ public class ServicioControler {
 		model.addAttribute("servicios", listaServicios.getServicios());
 		return "servicios-listado";
 	}
+	
+	@GetMapping("servicio/nuevo")
+	public String getNuevoServicio(Model model) {
+		boolean edicion = false;
+		model.addAttribute("servicio", new Servicio());
+		model.addAttribute("edicion", edicion);
+		return"servicios-nuevo";
+	}
+	
+	@PostMapping("servicio/guardar")
+	/* public model getGuardarProductoPage(@ModelAttribute("producto")Producto producto){ */
+	public String getGuardarServicioPage(@ModelAttribute("servicio")Servicio servicio){
+		ModelAndView modelView = new ModelAndView("servicios");
+		listaServicios.getServicios().add(servicio);
+		modelView.addObject("servicios", listaServicios.getServicios());
+		/*return model;*/
+		return"redirect:/servicio/listado";
+	}
+	
+	@GetMapping("servicio/modificar/{codigoServ}")
+	public String getModificarServicioPage(Model model, @PathVariable(value = "codigoServ") int codigoServ) {
+		Servicio servicioEncontrado = new Servicio();
+		boolean edicion=true;
+		for(Servicio servi : listaServicios.getServicios()){
+			if(servi.getCodigoServ() == codigoServ) {
+				servicioEncontrado = servi;
+				break;
+			}
+		}
+		model.addAttribute("servicio", servicioEncontrado);
+		model.addAttribute("edicion", edicion);
+		return"servicios-nuevo";
+	}	
+	
+	@PostMapping("servicio/modificar")
+	public String modificarServicio(@ModelAttribute("servicio") Servicio servicio) {
+		for(Servicio servic : listaServicios.getServicios()) {
+			if(servic.getCodigoServ() == servicio.getCodigoServ()) {
+				servic.setNombreServ(servicio.getNombreServ());
+				servic.setDescripcionServ(servicio.getDescripcionServ());
+				servic.setCategoriaServ(servicio.getCategoriaServ());
+				servic.setPrecioServ(servicio.getPrecioServ());
+				break;
+			}
+		}
+		return "redirect:/servicio/listado";
+	}
+	
 }
