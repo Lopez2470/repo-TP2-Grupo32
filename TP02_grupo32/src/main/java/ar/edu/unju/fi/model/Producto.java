1ef767eada3ec0e8905dd34697463e7199365e59
GabrielaLopez <GTL@DESKTOP-0LQB8T5>
2023-06-04 18:02:14 -0300

    Modificacion clase producto, controller y html para validacion: Modificacion de clase Producto agregando las anotaciones para la validacion. Se modificó el ProductoController para que realice la validacion. Archivos html fueron adaptados para las validaciones.

--- TP02_grupo32/src/main/java/ar/edu/unju/fi/model/Producto.java
+++ TP02_grupo32/src/main/java/ar/edu/unju/fi/model/Producto.java
@@ -2,12 +2,28 @@ package ar.edu.unju.fi.model;
 
 import org.springframework.stereotype.Component;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 @Component
 public class Producto {
+	@Size(min = 5,message = "Minimo de caracteres permitidos para el Nombre es 5")
+	@Size(max = 20, message = "Máximo de caracteres permitidos para un Nombre es 20")
 	private String nombreProducto;
+	@Min(value = 80000,message = "El minimo valor del código es 80000")
+	@Max(value = 89999, message = "El máximo valor del código es 89999")
 	private int codigoProducto;
+	
+	@Min(value = 1,message = "Ingrese un valor del Producto")
 	private double precioProducto;
+	@NotBlank(message = "Seleccione una categoria")
 	private String categoriaProducto;
+	@Min(value = 1,message = "El Mínimo porcentaje de descuento es 1 ")
+	@Max(value = 99, message = "El Máximo porcentaje de descuento es 99")
 	private int descuentoProducto;
 	
 	public Producto() {
