b79b6ac4d2a4ce7f062fa923a67913d1ec1a18e6
David Paoloni <Rx@ASUSRX>
2023-06-11 12:18:06 -0300

    Crear interface e implementación: Crear la interface IServicioService para la entidad Servicio y únicamente definir los métodos vinculados. Crear la implementación ServicioService en el cual se desarrollan los métodos adquiridos por contrato de interface.

---
+++ TP02_grupo32/src/main/java/ar/edu/unju/fi/service/IServicioService.java
@@ -0,0 +1,25 @@
+package ar.edu.unju.fi.service;
+
+import java.util.List;
+
+import ar.edu.unju.fi.model.Servicio;
+
+public interface IServicioService {
+	
+	//Métodos abstractos de la entidad Servicio
+	//Retorna los objetos Servicio
+	List<Servicio> getServicios();
+	//Retorna el objeto Servicio
+	Servicio getServicio();
+	//Guarda el nuevo objeto Servicio
+	void guardarServ(Servicio servicio);
+	//Busca un objeto por el atributo codigo de servicio
+	public Servicio buscarServicioById(int id);
+	//Modifica un objeto Servicio
+	void modificarServ(Servicio servicio);
+	//Elimina un elemento Servicio
+	void eliminarServ(int id);
+	//Busca un objeto por el atributo nombre de servicio
+	public Servicio buscarServicioByNombre(String nombre);
+	
+}
