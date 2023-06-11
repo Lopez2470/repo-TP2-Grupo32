6cdc77559925e31ffbc1510b35d020f07ca43a40
Carlos <Carlos@DESKTOP-LONCR8V>
2023-06-11 11:33:01 -0300

    Se agregaron comentarios en IConsejoService, ConsejoServiceImp y en ConsejoController.
    Se agregó en ConsejoController y consejos-nuevo.html el incremento
    automatico de los codigos de consejo.

--- TP02_grupo32/src/main/java/ar/edu/unju/fi/service/IConsejoService.java
+++ TP02_grupo32/src/main/java/ar/edu/unju/fi/service/IConsejoService.java
@@ -5,18 +5,19 @@ import java.util.List;
 import ar.edu.unju.fi.model.Consejo;
 
 public interface IConsejoService {
-	
+	//Definicion de los Metodos abstractos, sin definición.
+	//Devuelve la lista de objetos consejo.
 	List<Consejo> getConsejos();
-	
+	//Devuelve el objeto consejo
 	Consejo getConsejo();
-	
+	//Guarda el objeto consejo
 	void guardarConsejo(Consejo consejo);
-	
+	//Realiza la busqueda de un objeto por el atributo codigoConsejo.
 	public Consejo buscarConsejoById(int id);
-	
+	//Guarda las modificaciones realizadas al objeto
 	void modificarConsejo(Consejo consejo);
-	
+	// Elimina el objeto seleccionado en el listado de consejos
 	void eliminarConsejo(int id);
-	
+	//Realiza la busqueda de un objeto consejo por el atributo titulo.
 	public Consejo buscarConsejoByTitulo(String titulo);
 }
--- TP02_grupo32/src/main/java/ar/edu/unju/fi/service/imp/ConsejoServiceImp.java
+++ TP02_grupo32/src/main/java/ar/edu/unju/fi/service/imp/ConsejoServiceImp.java
@@ -10,28 +10,42 @@ import ar.edu.unju.fi.model.Consejo;
 import ar.edu.unju.fi.service.IConsejoService;
 @Service
 public class ConsejoServiceImp implements IConsejoService{
-
+	
 	@Autowired
 	private ListaConsejo listaConsejos;
 	
 	@Autowired
 	private Consejo consejo;
-	
+	/**
+	 * Metodo que devuelve la lista de consejos.
+	 */
 	@Override
 	public List<Consejo> getConsejos() {
 		return listaConsejos.getConsejos();
 	}
-
+	/**
+	 * Metodo que devuelve el objeto consejo
+	 */
 	@Override
 	public Consejo getConsejo() {
 		return consejo;
 	}
-
+	/**
+	* Metodo que guarda el objeto enviado como parametro
+	* Recibe un objeto validado
+	* @param consejo es el objeto de Consejo enviado como parametro
+	* @return 
+	*/
 	@Override
 	public void guardarConsejo(Consejo consejo) {
 		getConsejos().add(consejo);
 	}
-	
+	/**
+	* Metodo que realiza la busqueda de un objeto
+	* La busqueda la realiza comparando con el codigo de consejo enviado como parametro
+	* @param recibe como parametro la variable int id que es el codigo de consejo
+	* @return retorna el objeto encontrado a travez de su codigo de consejo enviado.
+	*/
 	@Override
 	public Consejo buscarConsejoById(int id) {
 		Consejo consejoAuxiliar = new Consejo();
@@ -43,7 +57,11 @@ public class ConsejoServiceImp implements IConsejoService{
 		}
 		return consejoAuxiliar;
 	}
-
+	/** Metodo que guarda el objeto modificado enviado como parametro
+	* Recibe un objeto con los nuevos valores de los atributos
+	* @param objeto que tiene las modificaciones realizadas
+	* @return 
+	*/
 	@Override
 	public void modificarConsejo(Consejo consejo) {
 		Consejo consejoAModificar = buscarConsejoById(consejo.getCodigoConsejo());
@@ -53,13 +71,22 @@ public class ConsejoServiceImp implements IConsejoService{
 		consejoAModificar.setAutorConsejo(consejo.getAutorConsejo());
 		consejoAModificar.setCategoriaConsejo(consejo.getCategoriaConsejo());
 	}
-
+	/** Metodo que elimina el objeto enviado 
+	* Recibe el codigo de consejo para buscar enla lista y luego eliminarlo.
+	* @param el codigo de consejo que sera eliminado.
+	* @return 
+	*/
 	@Override
 	public void eliminarConsejo(int id) {
 		Consejo consejoAEliminar = buscarConsejoById(id);
 		getConsejos().remove(consejoAEliminar);
 	}
-
+	/**
+	* Metodo que realiza la busqueda de un objeto
+	* La busqueda la realiza comparando con el titulo de consejo enviado como parametro
+	* @param recibe como parametro la variable String titulo del consejo
+	* @return retorna el objeto encontrado a travez del titulo de consejo enviado.
+	*/
 	@Override
 	public Consejo buscarConsejoByTitulo(String titulo) {
 		Consejo consejoAuxiliar = new Consejo();
