f797de6333a10a2d485fb98e0171d39e29268af4
Enamuel L <Enamuel L@EmAnUeL>
2023-06-25 23:08:07 -0300

    se agrego las interfaces necesarias para la clase provincia

---
+++ TP02_grupo32/src/main/java/ar/edu/unju/fi/repository/IProvinciaRepository.java
@@ -0,0 +1,9 @@
+package ar.edu.unju.fi.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import ar.edu.unju.fi.entity.Provincia;
+
+public interface IProvinciaRepository extends CrudRepository<Provincia,Long> {
+	
+}
---
+++ TP02_grupo32/src/main/java/ar/edu/unju/fi/service/IProvinciaService.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.service;
+
+import java.util.List;
+
+import ar.edu.unju.fi.entity.Provincia;
+
+
+
+public interface IProvinciaService {
+
+		//retorna los objetos provincia a listar.
+		List<Provincia> getProvincias();
+}
